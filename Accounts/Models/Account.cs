b4a5b66001849a2afe2e1e2218b38299a1e6ed98
Karam <krm-011@hotmail.com>
2022-12-22 01:00:18 +0100

    Added Paket, connected it to a repo with theater lib

--- Accounts/Models/Account.cs
+++ Accounts/Models/Account.cs
@@ -2,16 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TheaterLaak;
 
-
-    public class Account : Klant
+public class Account : Klant
     {   
         public int Id { get; set; }
         public string naam { get; set; }
         public string email { get; set; }
         public string wachtwoord { get; set; }
-        // public List<Genre> Intresses { get; set; }
-        // public List<Kaart> AankoopGeschiedenis { get; set; }
+        public List<IGenre> Intresses { get; set; }
+        public List<IKaart> AankoopGeschiedenis { get; set; }
 
         public void inloggen(string email, string wachtwoord)
         {
@@ -21,14 +21,14 @@ using System.Threading.Tasks;
         {
             // Voegt bedrag toe aan de donatie pot
         }
-        // public void kaartOverzetten(Kaart kaart, Account account)
-        // {
-        //     // Verplaatst kaart van de ene account naar de andere
-        // }
-        // public void setIntresse(List<Genre> intresses)
-        // {
-        //     // Zet de intresses van de account
-        // }
+        public void kaartOverzetten(Kaart kaart, Account account)
+        {
+            // Verplaatst kaart van de ene account naar de andere
+        }
+        public void setIntresse(List<Genre> intresses)
+        {
+            // Zet de intresses van de account
+        }
         public void resetWachtwoord(string email)
         {
             // Stuurt een email met een link om het wachtwoord te resetten
