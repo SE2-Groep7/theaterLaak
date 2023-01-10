8f3633db7025272a2587046ac72b562db7ec1bea
Karam <krm-011@hotmail.com>
2023-01-10 13:25:47 +0100

    Voorstellingen pagina gemaakt, bug fix in home pagina, navbar fix card fix

--- React frontend/src/components/Home/Card.js
+++ React frontend/src/components/Home/Card.js
@@ -1,11 +1,11 @@
 const Card = (props) => {
     return (
-        <div class="show">
+        <div class={props.class + " show"}>
                     <img src={props.foto} alt={props.fotoAlt} />
                      <div class="info">  
                     <h2>{props.beschrijving}</h2>
-                    <p>asdasdasd</p>
-                    <button class="show-btn">Buy Tickets</button>
+                    <p>Dit is een placeholder voor de show verhaaltje</p>
+                    <div class="btn-holder"><button class="show-btn">Buy Tickets</button></div>
                     </div>
                     
         </div>
