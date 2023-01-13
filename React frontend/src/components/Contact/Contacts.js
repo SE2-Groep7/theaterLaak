facc0848a5e20d80efd0857c5c77356ded6290cb
Lionel <lionelmohansingh@gmail.com>
2023-01-13 15:52:25 +0100

    Statische pagina's Contact, over ons, cookies, disclaimer en privacy PrivacyStatement

--- React frontend/src/components/Contact/Contacts.js
+++ React frontend/src/components/Contact/Contacts.js
@@ -1,6 +1,24 @@
-const Contact = () => {
-    return <h1>Contact Me</h1>;
-  };
-  
+import React from 'react';
+import "./contact.css";
+import foto1 from "./images/pexels-pixabay-63328.jpg";
+
+function Contact() {
+  return (
+    <div className="contact">
+      <div className="contact-title">
+      <h2>Contact</h2>
+      </div>
+        <div className="contact-text">
+            <p>Theater Laak is gelegen in het hart van het Laakkwartier en is gemakkelijk te bereiken. Of u nu met de auto, openbaar vervoer of te voet komt, wij zijn gemakkelijk te vinden.</p>
+                <p>Voor vragen, opmerkingen of reserveringen kunt u contact met ons opnemen via onze contactpagina op onze website, een e-mail sturen naar <a href="mailto: info@theaterlaak.nl">info@theaterlaak.nl</a>, of telefonisch contact opnemen via <a href="tel: (070)-44588889">(070)-44588889</a>. Onze klantenservice is beschikbaar van maandag tot en met vrijdag tussen 9:00 en 17:00 uur.</p>
+                <p>Als u liever uw vraag of opmerking per post wilt sturen, kunt u dat doen naar Westerdijkplein 750, 25212 EN Den Haag of kom gerust een keer langs bij ons.</p>
+                <p>We doen ons best om u zo snel mogelijk te helpen en zullen u binnen 24 uur terugbellen of antwoorden op uw e-mail. Bedankt voor het contact met Theater Laak.</p>
+        </div>
+        <div className="contact-images">
+        <img src={foto1} alt="Contact Foto1"/>
+        </div>
+    </div>
+  );
+};
+
   export default Contact;
-  
--- React frontend/src/components/OverOns/OverOns.js
+++ React frontend/src/components/OverOns/OverOns.js
@@ -1,6 +1,31 @@
-const OverOns = () => {
-    return <h1>Over Ons</h1>;
-  };
-  
+import React from 'react';
+import "./overons.css";
+import foto1 from "./images/pexels-francesco-ungaro-669319.jpg";
+import foto2 from "./images/pexels-kosygin-leishangthem-2888802.jpg";
+import foto3 from "./images/pexels-matheus-viana-2372945.jpg";
+import foto4 from "./images/pexels-pixabay-45258.jpg";
+
+
+function OverOns() {
+  return (
+    <div className="about-us">
+      <div className="about-us-title">
+      <h2>Over Ons</h2>
+      </div>
+      <div className="about-us-text">
+        <p>Theater Laak is een cultureel centrum voor de hele gemeenschap van het Laakkwartier en omgeving. Wij zijn trots om een breed scala aan optredens en evenementen aan te bieden, van toneelstukken tot live muziek en comedy.</p>
+        <p>Met onze nieuwe webapplicatie kunt u gemakkelijk kaartjes kopen voor onze shows en onze programmering bekijken. Organisaties en individuen kunnen ook zalen reserveren voor eigen evenementen.</p>
+        <p>Onze trouwe groep donateurs speelt een belangrijke rol in het behoud van ons culturele programma en de toegankelijkheid ervan voor de gemeenschap. Daarom bieden we een speciaal begunstigersportaal met exclusieve content en voorrang bij kaartverkoop voor onze donateurs.</p>
+        <p>We danken onze publiek en donateurs voor hun steun en kijken ernaar uit om u binnenkort te verwelkomen in Theater Laak.</p>
+      </div>
+      <div className="about-us-images">
+        <img src={foto1} alt="Over Ons Foto1"/>
+        <img src={foto2} alt="Over Ons Foto2"/>
+        <img src={foto3} alt="Over Ons Foto3"/>
+        <img src={foto4} alt="Over Ons Foto4"/>
+      </div>
+    </div>
+  );
+};
+
   export default OverOns;
-  
