7bfddd873fb7a7c2755757a3765ab8fd82599ec0
Lionel <lionelmohansingh@gmail.com>
2023-01-16 12:22:12 +0100

    Gebruikersportaal nieuw

--- React frontend/src/components/GebruikersPortaal/GebruikersPortaal.js
+++ React frontend/src/components/GebruikersPortaal/GebruikersPortaal.js
@@ -1,4 +1,5 @@
 import "./GebruikersPortaal.css";
+import {Link} from 'react-router-dom'
 
 const GebruikersPortaal = () => {
   return (
@@ -8,20 +9,26 @@ const GebruikersPortaal = () => {
         <h2 class="GebruikersPortaalTitle">GebruikersPortaal</h2>
           <div class="GebruikersPortaalContentBox">
             <div class="GebruikersPortaalContentWrapper">
-              <h2 class="GebruikersPortaalContentTitle">Uw Laatste bezoeken</h2>
-            </div>
-            <div class="GebruikersPortaalContentWrapper">
               <h2 class="GebruikersPortaalContentTitle">Persoonlijke Agenda</h2>
-            </div>
-            <div class="GebruikersPortaalContentWrapper">
-              <h2 class="GebruikersPortaalContentTitle">Tickets Overzetten</h2>
+                <div class="GebruikersPortaalField">
+                <Link to="/voorstelling">
+                  <button type="submit">Judeska Airlines</button>
+                  </Link>
+                </div>
             </div>
             <div class="GebruikersPortaalContentWrapper">
               <h2 class="GebruikersPortaalContentTitle">Doneren</h2>
             </div>
             <div class="GebruikersPortaalContentWrapper">
-              <h2 class="GebruikersPortaalContentTitle">Accountgegevens wijzigen</h2>
+              <h2 class="GebruikersPortaalContentTitle">Aankoophistorie</h2>
             </div>
+            <div class="GebruikersPortaalContentWrapper">
+              <h2 class="GebruikersPortaalContentTitle">Tickets Overzetten</h2>
+            </div>
+            <div class="GebruikersPortaalContentWrapper">
+              <h2 class="GebruikersPortaalContentTitle">BegunstigersPortaal</h2>
+            </div>
+            
             <div class="GebruikersPortaalContentWrapper">
               <h2 class="GebruikersPortaalContentTitle">Uitloggen</h2>
             </div>
