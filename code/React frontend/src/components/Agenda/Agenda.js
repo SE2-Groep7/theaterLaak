6d8cc8c6f736ec799d653b20aefa64998740904e
Karam <krm-011@hotmail.com>
2023-01-19 01:19:45 +0100

    fixed websocket ip address

--- code/React frontend/src/components/Agenda/Agenda.js
+++ code/React frontend/src/components/Agenda/Agenda.js
@@ -64,7 +64,7 @@ const Agenda = () => {
   };
     useEffect(() => {
       getShows(value);
-      var connection = new signalR.HubConnectionBuilder().withUrl("http://localhost:5245/showhub").build()
+      var connection = new signalR.HubConnectionBuilder().withUrl("http://mohieddin.nl/showapi/showhub").build()
       connection.on("newShow", handleNewShow);
       connection.start().then(() => {
         setConnection(connection);
